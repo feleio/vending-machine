7908f37135eeb5683f11abcb54ef418d9da49a2e
feleio <lokling@gmail.com>
2020-04-25 16:09:27 +0100

    add all coins value as config

--- src/main/scala/io/fele/vending_machine/model/Coin.scala
+++ src/main/scala/io/fele/vending_machine/model/Coin.scala
@@ -3,3 +3,7 @@ package io.fele.vending_machine.model
 // value unit pence
 case class Coin(value: Int)
 case class CoinCount(coin: Coin, count: Int)
+
+object Coin {
+  val allCoins: Set[Int] = Set(1, 2, 5, 10, 20, 50, 100, 200)
+}
