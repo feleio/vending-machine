f57d7fdffc3e4c6b30b9f0233179581a3eddfb60
feleio <lokling@gmail.com>
2020-04-25 20:24:04 +0100

    implement Vending Machine Service

--- src/main/scala/io/fele/vending_machine/VendingMachineService.scala
+++ src/main/scala/io/fele/vending_machine/VendingMachineService.scala
@@ -1,6 +1,13 @@
 package io.fele.vending_machine
 
-import io.fele.vending_machine.model.{CoinCount, ProductCount, ReturnedProductAndChange}
+import io.fele.vending_machine.model.{CoinCount, Product, ProductCount, ReturnedProductAndChange}
+import io.fele.vending_machine.repo.{CoinsRepo, ProductsRepo, VendingStateRepo}
+import io.fele.vending_machine.ChangeCalculation.RichCoinCountList
+
+sealed trait VendingMachineException extends Exception
+case object ProductNotFoundException extends VendingMachineException
+case object NotEnoughCoinsForChange extends VendingMachineException
+case object NotEnoughCoinInserted extends VendingMachineException
 
 trait VendingMachineService {
   // read only actions
@@ -8,7 +15,9 @@ trait VendingMachineService {
 
   def listAvailableProduct: List[ProductCount]
 
-  def getSelectedProduct: Option[Int]
+  def getSelectedProductId: Option[Int]
+
+  def getProduct(productId: Int): Option[Product]
 
   def getInsertedCoins: List[CoinCount]
 
@@ -17,7 +26,80 @@ trait VendingMachineService {
 
   def loadProducts(products: List[ProductCount]): Unit
 
-  def selectProduct(productId: Int): Option[ReturnedProductAndChange]
+  def selectProduct(productId: Int): Either[VendingMachineException, Option[ReturnedProductAndChange]]
 
-  def insertCoin(coinCounts: List[CoinCount]): Option[ReturnedProductAndChange]
+  def insertCoins(coinCounts: List[CoinCount]): Either[VendingMachineException, Option[ReturnedProductAndChange]]
+}
+
+class VendingMachineServiceImpl(
+  val coinsRepo: CoinsRepo,
+  val productsRepo: ProductsRepo,
+  val vendingStateRepo: VendingStateRepo,
+) extends VendingMachineService {
+  override def listAvailableCoins: List[CoinCount] =
+    coinsRepo.listCoins
+
+  override def listAvailableProduct: List[ProductCount] =
+    productsRepo.listProductCounts
+
+  override def getSelectedProductId: Option[Int] =
+    vendingStateRepo.getSelectedProduct
+
+  override def getProduct(productId: Int): Option[Product] =
+    productsRepo.getProduct(productId)
+
+  override def getInsertedCoins: List[CoinCount] =
+    vendingStateRepo.getInsertedCoins
+
+  override def loadCoins(coinCounts: List[CoinCount]): Unit =
+    coinsRepo.loadCoins(coinCounts)
+
+  override def loadProducts(products: List[ProductCount]): Unit =
+    productsRepo.loadProducts(products)
+
+  override def selectProduct(productId: Int): Either[VendingMachineException, Option[ReturnedProductAndChange]] = {
+    val productOpt: Option[Product] = getProduct(productId)
+
+    productOpt match {
+      case Some(product) =>
+        vendingStateRepo.selectProduct(product.id)
+        outputProductAndChange()
+      case None =>
+        Left(ProductNotFoundException)
+    }
+  }
+
+  override def insertCoins(coinCounts: List[CoinCount]): Either[VendingMachineException, Option[ReturnedProductAndChange]] = {
+    vendingStateRepo.insertCoins(coinCounts)
+    outputProductAndChange()
+  }
+
+  private def outputProductAndChange(): Either[VendingMachineException, Option[ReturnedProductAndChange]] = {
+    val selectedProduct: Option[Product] = getSelectedProductId.flatMap(getProduct)
+    val insertedCoins: List[CoinCount] = getInsertedCoins
+    val currentAmount: Int = insertedCoins.totalAmount
+
+    selectedProduct match {
+      case Some(product) =>
+        if (currentAmount >= product.price) {
+          coinsRepo.loadCoins(insertedCoins)
+          val changeOpt: Option[List[CoinCount]] = ChangeCalculation.getChange(listAvailableCoins, currentAmount - product.price)
+          changeOpt match {
+            case Some(change) =>
+              productsRepo.removeProduct(product.id)
+              coinsRepo.removeCoins(change)
+              vendingStateRepo.clear()
+              val output = ReturnedProductAndChange(product, change)
+              Right(Some(output))
+            case None =>
+              coinsRepo.removeCoins(insertedCoins)
+              Left(NotEnoughCoinsForChange)
+          }
+        }
+        else
+          Left(NotEnoughCoinInserted)
+      case None =>
+        Right(None)
+    }
+  }
 }
