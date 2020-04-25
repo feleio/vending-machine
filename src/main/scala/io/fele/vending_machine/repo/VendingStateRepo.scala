421b6b11701cd85b50d50fa2339371dbae18a979
feleio <lokling@gmail.com>
2020-04-25 17:35:26 +0100

    implement Vending State Repo

--- src/main/scala/io/fele/vending_machine/repo/VendingStateRepo.scala
+++ src/main/scala/io/fele/vending_machine/repo/VendingStateRepo.scala
@@ -1,11 +1,46 @@
 package io.fele.vending_machine.repo
 
-trait VendingStateRepo {
-  def getInsertedCoin: List[CoinsRepo]
-  def getSelectedProduct: Int
+import io.fele.vending_machine.model.{Coin, CoinCount}
 
-  def insertCoins(coins: List[CoinsRepo]): Unit
+import scala.collection.mutable
+
+trait VendingStateRepo {
+  def getInsertedCoins: List[CoinCount]
+  def getSelectedProduct: Option[Int]
+
+  def insertCoins(coins: List[CoinCount]): Unit
   def selectProduct(productId: Int): Unit
   def clear(): Unit
 }
 
+class InMemoryVendingStateRepo extends VendingStateRepo {
+  var currentProductId: Option[Int] = None
+  val coinsCountMap : mutable.HashMap[Int, Int] = mutable.HashMap(
+    Coin.allCoins.map { value =>
+      value -> 0
+    }.toSeq:_*
+  )
+  
+  override def getInsertedCoins: List[CoinCount] = coinsCountMap.collect {
+    case (value, count) if count > 0 => CoinCount(Coin(value), count)
+  }.toList.sortBy(_.coin.value)(Ordering.Int.reverse)
+
+  override def getSelectedProduct: Option[Int] = currentProductId
+
+  override def insertCoins(coins: List[CoinCount]): Unit = {
+    for {
+      coinCount <- coins
+    } {
+      coinsCountMap(coinCount.coin.value) += coinCount.count
+    }
+  }
+
+  override def selectProduct(productId: Int): Unit = {
+    currentProductId = Some(productId)
+  }
+
+  override def clear(): Unit = {
+    currentProductId = None
+    coinsCountMap.clear()
+  }
+}
