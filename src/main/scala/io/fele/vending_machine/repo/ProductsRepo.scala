9a963ef50a211f7054514ede833367b8cf1675f3
feleio <lokling@gmail.com>
2020-04-25 16:55:53 +0100

    product repo implementation

--- src/main/scala/io/fele/vending_machine/repo/ProductsRepo.scala
+++ src/main/scala/io/fele/vending_machine/repo/ProductsRepo.scala
@@ -1,24 +1,47 @@
 package io.fele.vending_machine.repo
 
-import io.fele.vending_machine.model.ProductCount
+import io.fele.vending_machine.model.{Coin, ProductCount}
+
+import scala.collection.mutable
 
 sealed trait ProductException extends Exception
 case object ProductNotFoundException extends ProductException
 
 trait ProductsRepo {
   def listProducts: List[ProductCount]
-  def getProductCount(productId: Int): ProductCount
+  def getProductCount(productId: Int): Int
   def loadProducts(productCounts: List[ProductCount]): Unit
   def removeProduct(productId: Int): Either[ProductException, Unit]
 }
 
 class InMemoryProductsRepo extends ProductsRepo {
-  override def listProducts: List[ProductCount] = ???
+  val productCountMap : mutable.HashMap[Int, Int] = mutable.HashMap()
 
-  override def getProductCount(productId: Int): ProductCount = ???
+  override def listProducts: List[ProductCount] = productCountMap.collect {
+    case (productId, count) if count > 0 => ProductCount(productId, count)
+  }.toList.sortBy(_.productId)
 
-  override def loadProducts(productCounts: List[ProductCount]): Unit = ???
+  override def getProductCount(productId: Int): Int = productCountMap.getOrElse(productId, 0)
 
-  override def removeProduct(productId: Int): Either[ProductException, Unit] = ???
+  override def loadProducts(productCounts: List[ProductCount]): Unit = {
+    for {
+      productCount <- productCounts
+    } {
+      productCountMap.get(productCount.productId) match {
+        case Some(_) =>
+          productCountMap(productCount.productId) += productCount.count
+        case None =>
+          productCountMap(productCount.productId) = productCount.count
+      }
+    }
+  }
+
+  override def removeProduct(productId: Int): Either[ProductException, Unit] =
+    if (productCountMap.getOrElse(productId, 0) > 0) {
+      productCountMap(productId) -= 1
+      Right(())
+    }
+    else
+      Left(ProductNotFoundException)
 }
 
