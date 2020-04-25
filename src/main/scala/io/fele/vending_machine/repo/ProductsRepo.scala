98286cdd486bf47129e599483a63ab057fe86a1a
feleio <lokling@gmail.com>
2020-04-25 16:33:53 +0100

    revise product repo interface

--- src/main/scala/io/fele/vending_machine/repo/ProductsRepo.scala
+++ src/main/scala/io/fele/vending_machine/repo/ProductsRepo.scala
@@ -2,8 +2,23 @@ package io.fele.vending_machine.repo
 
 import io.fele.vending_machine.model.ProductCount
 
+sealed trait ProductException extends Exception
+case object ProductNotFoundException extends ProductException
+
 trait ProductsRepo {
   def listProducts: List[ProductCount]
   def getProductCount(productId: Int): ProductCount
   def loadProducts(productCounts: List[ProductCount]): Unit
+  def removeProduct(productId: Int): Either[ProductException, Unit]
 }
+
+class InMemoryProductsRepo extends ProductsRepo {
+  override def listProducts: List[ProductCount] = ???
+
+  override def getProductCount(productId: Int): ProductCount = ???
+
+  override def loadProducts(productCounts: List[ProductCount]): Unit = ???
+
+  override def removeProduct(productId: Int): Either[ProductException, Unit] = ???
+}
+
