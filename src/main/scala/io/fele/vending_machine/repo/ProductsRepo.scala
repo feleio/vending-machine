0be050f48c42b64db9f0f5f95199663152c0db88
feleio <lokling@gmail.com>
2020-04-25 18:08:58 +0100

    add product information repo

--- src/main/scala/io/fele/vending_machine/repo/ProductsRepo.scala
+++ src/main/scala/io/fele/vending_machine/repo/ProductsRepo.scala
@@ -1,6 +1,6 @@
 package io.fele.vending_machine.repo
 
-import io.fele.vending_machine.model.{Coin, ProductCount}
+import io.fele.vending_machine.model.{Product, ProductCount}
 
 import scala.collection.mutable
 
@@ -8,18 +8,22 @@ sealed trait ProductException extends Exception
 case object ProductNotFoundException extends ProductException
 
 trait ProductsRepo {
-  def listProducts: List[ProductCount]
+  def getProduct(productId: Int): Option[Product]
+  def listProductCounts: List[ProductCount]
   def getProductCount(productId: Int): Int
   def loadProducts(productCounts: List[ProductCount]): Unit
   def removeProduct(productId: Int): Either[ProductException, Unit]
 }
 
 class InMemoryProductsRepo extends ProductsRepo {
+  val productMap : mutable.HashMap[Int, Product] = mutable.HashMap()
   val productCountMap : mutable.HashMap[Int, Int] = mutable.HashMap()
 
-  override def listProducts: List[ProductCount] = productCountMap.collect {
-    case (productId, count) if count > 0 => ProductCount(productId, count)
-  }.toList.sortBy(_.productId)
+  override def getProduct(productId: Int): Option[Product] = productMap.get(productId)
+
+  override def listProductCounts: List[ProductCount] = productCountMap.collect {
+    case (productId, count) if count > 0 => ProductCount(productMap(productId), count)
+  }.toList.sortBy(_.product.id)
 
   override def getProductCount(productId: Int): Int =
     productCountMap.getOrElse(productId, 0)
@@ -28,11 +32,12 @@ class InMemoryProductsRepo extends ProductsRepo {
     for {
       productCount <- productCounts
     } {
-      productCountMap.get(productCount.productId) match {
+      productMap(productCount.product.id) = productCount.product
+      productCountMap.get(productCount.product.id) match {
         case Some(_) =>
-          productCountMap(productCount.productId) += productCount.count
+          productCountMap(productCount.product.id) += productCount.count
         case None =>
-          productCountMap(productCount.productId) = productCount.count
+          productCountMap(productCount.product.id) = productCount.count
       }
     }
   }
