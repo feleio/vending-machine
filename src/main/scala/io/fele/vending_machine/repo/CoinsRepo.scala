b083e0a3c68d4cf71da815f514a290d5eb26ae55
feleio <lokling@gmail.com>
2020-04-25 15:49:18 +0100

    revise coins interface

--- src/main/scala/io/fele/vending_machine/repo/CoinsRepo.scala
+++ src/main/scala/io/fele/vending_machine/repo/CoinsRepo.scala
@@ -2,14 +2,21 @@ package io.fele.vending_machine.repo
 
 import io.fele.vending_machine.model.CoinCount
 
+sealed trait CoinsException
+case object NotEnoughCoinsException extends Exception
+
+
 trait CoinsRepo {
   def getCoins: List[CoinCount]
   def loadCoins(coinCounts: List[CoinCount]): Unit
+  def removeCoins(coinCounts: List[CoinCount]): Either[CoinsException, Unit]
 }
 
 class InMemoryCoinsRepo extends CoinsRepo {
   override def getCoins: List[CoinCount] = ???
 
   override def loadCoins(coinCounts: List[CoinCount]): Unit = ???
+
+  override def removeCoins(coinCounts: List[CoinCount]): Either[CoinsException, Unit] = ???
 }
 
