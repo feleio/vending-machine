e27588e3dbf7986c6a5e99583fea0e629f82e158
feleio <lokling@gmail.com>
2020-04-25 15:29:51 +0100

    add Vending Machine interface

---
+++ src/main/scala/io/fele/vending_machine/VendingMachine.scala
@@ -0,0 +1,19 @@
+package io.fele.vending_machine
+
+import io.fele.vending_machine.model.{CoinCount, ProductCount}
+
+trait VendingMachine {
+  def getAvailableCoins: List[CoinCount]
+
+  def getAvailableProduct: List[ProductCount]
+
+  def getSelectedProduct: List[Product]
+
+  def loadCoins(coinCounts: List[CoinCount]): Unit
+
+  def loadProducts(products: List[ProductCount]): Unit
+
+  def selectProduct(productId: Int): Unit
+
+  def insertCoins(coinCounts: List[CoinCount]): Unit
+}
