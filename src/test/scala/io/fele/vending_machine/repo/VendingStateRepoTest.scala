db59c6b8893d91f04bac4e452aa2e305fc7dd499
feleio <lokling@gmail.com>
2020-04-25 17:35:09 +0100

    adding vending state Repo test

---
+++ src/test/scala/io/fele/vending_machine/repo/VendingStateRepoTest.scala
@@ -0,0 +1,81 @@
+package io.fele.vending_machine.repo
+
+import io.fele.vending_machine.model.{Coin, CoinCount}
+import org.scalatest.freespec.AnyFreeSpec
+import org.scalatest.matchers.should.Matchers
+
+class VendingStateRepoTest extends AnyFreeSpec with Matchers {
+  val coins = List(
+    CoinCount(Coin(200), count = 2),
+    CoinCount(Coin(100), count = 2),
+    CoinCount(Coin(50), count = 2),
+    CoinCount(Coin(20), count = 2),
+    CoinCount(Coin(10), count = 2),
+    CoinCount(Coin(5), count = 2),
+    CoinCount(Coin(2), count = 2),
+    CoinCount(Coin(1), count = 2),
+  )
+
+  val vendingStateRepo = new InMemoryVendingStateRepo()
+  val vendingStateRepoWithCoins = new InMemoryVendingStateRepo()
+  vendingStateRepoWithCoins.insertCoins(coins)
+
+  "VendingStateRepo" - {
+    "when contains 0 coins and no selected Product" - {
+      vendingStateRepo.getInsertedCoins should be (Nil)
+      vendingStateRepo.getSelectedProduct should be (None)
+
+      "should be able to insert 0 coins" in {
+        val zeroCoins = Nil
+        vendingStateRepo.insertCoins(zeroCoins)
+        vendingStateRepo.getInsertedCoins should be (Nil)
+      }
+
+      "should be able to load coins" in {
+        vendingStateRepo.insertCoins(coins)
+        vendingStateRepo.getInsertedCoins should be (coins)
+      }
+
+      "should be able to select product" in {
+        vendingStateRepo.getSelectedProduct should be (None)
+        vendingStateRepo.selectProduct(productId = 1)
+        vendingStateRepo.getSelectedProduct should be (Some(1))
+        vendingStateRepo.selectProduct(productId = 2)
+        vendingStateRepo.getSelectedProduct should be (Some(2))
+      }
+    }
+
+    "when contains many coins" - {
+      vendingStateRepoWithCoins.getInsertedCoins should be (coins)
+      vendingStateRepoWithCoins.getSelectedProduct should be (None)
+
+      "should be able to insert more coins" in {
+        vendingStateRepoWithCoins.getInsertedCoins should be (coins)
+        vendingStateRepoWithCoins.insertCoins(coins)
+        vendingStateRepoWithCoins.getInsertedCoins should be (List(
+          CoinCount(Coin(200), count = 4),
+          CoinCount(Coin(100), count = 4),
+          CoinCount(Coin(50), count = 4),
+          CoinCount(Coin(20), count = 4),
+          CoinCount(Coin(10), count = 4),
+          CoinCount(Coin(5), count = 4),
+          CoinCount(Coin(2), count = 4),
+          CoinCount(Coin(1), count = 4),
+        ))
+        vendingStateRepoWithCoins.insertCoins(List(
+          CoinCount(Coin(200), count = 1),
+        ))
+        vendingStateRepoWithCoins.getInsertedCoins should be (List(
+          CoinCount(Coin(200), count = 5),
+          CoinCount(Coin(100), count = 4),
+          CoinCount(Coin(50), count = 4),
+          CoinCount(Coin(20), count = 4),
+          CoinCount(Coin(10), count = 4),
+          CoinCount(Coin(5), count = 4),
+          CoinCount(Coin(2), count = 4),
+          CoinCount(Coin(1), count = 4),
+        ))
+      }
+    }
+  }
+}
